a1eee29c29717ca73aaac13810590211f3955b7a
ochi <ocos54ce@gmail.com>
2024-02-03 21:37:27 +0900

    first commit

---
+++ project/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),  # accounts.urls.pyを読み込むための設定を追加
+]
