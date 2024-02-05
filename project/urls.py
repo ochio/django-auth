07a5e1825c534027ee6d76c39c39b7936bc6029e
ochi <ocos54ce@gmail.com>
2024-02-05 19:11:57 +0900

    delete

--- project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("accounts.urls")),  # accounts.urls.pyを読み込むための設定を追加
-]
